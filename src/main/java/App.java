07c21357777d57a036ea58e85ecfe97789b8829b
Dave Gershman <davegershman465@gmail.com>
2021-09-12 17:33:17 -0400

    initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Dave Gershman
+ */
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is the quote? ");
+        String quote = scanner.nextLine();
+
+        System.out.print("Who said it? ");
+        String author = scanner.nextLine();
+
+        System.out.print(author + " says, " + "\"" + quote + "\"");
+    }
+}
